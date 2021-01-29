1ff46c5f6bfbd4d1edd4f9017705a10b8988f8f9
Leonardo Emili <leonardo.emili3@gmail.com>
2021-01-29 20:14:41 +0100

    Remove include/ directory

--- include/utility.hpp
+++
@@ -1,9 +0,0 @@
-#include <dirent.h>
-#include <opencv2/core/utility.hpp>
-
-using namespace cv;
-using namespace std;
-
-vector<string> readDataset(char*);
-string exec(const char*);
-int getDatasetSize(char*);

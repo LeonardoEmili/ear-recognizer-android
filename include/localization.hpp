bf57c44ec8c9711dc56102e41c54e857d7725012
Leonardo Emili <leonardo.emili3@gmail.com>
2021-01-29 18:44:28 +0100

    Add missing headers

---
+++ include/localization.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/objdetect.hpp>
+
+using namespace cv;
+using namespace std;
+
+/**
+ * Crops images by localizing ears and excluding
+ * external part.
+ */
+void crop_ears();
+
+void localize_ear();
+
+void initializeCascade(CascadeClassifier&, String);
